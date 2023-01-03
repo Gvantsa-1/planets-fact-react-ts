c28b7f117d5dd1a4b599843279d8e36c766571f3
gvantsa kakhniashvili <kakhniashviligvantsa@yahoo.com>
2023-01-04 00:38:39 +0400

    begining of pr

---
+++ src/App.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+import styled from "styled-components";
+function App() {
+  return <Container></Container>;
+}
+
+const Container = styled.div`
+  width: 327px;
+`;
+
+export default App;
